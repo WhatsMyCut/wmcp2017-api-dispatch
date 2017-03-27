0a93645ef942fc4daa05b0ce85caa4907ded3f23
Mike Taylor <mike@whatsmycut.com>
2017-03-26 20:58:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# wmcp2017-api-dispatch
+
+A quick and dirty PHP API using dispatch.php light-weight router.
+
+(https://github.com/noodlehaus/dispatch)[https://github.com/noodlehaus/dispatch]
+
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+require './dispatch.php';
+
+# create a stack of actions
+$routes = [
+  action('GET', '/users.json', function ($db, $config) {
+    $list = loadAllBooks($db);
+    $json = json_encode($list);
+    return response($json, 200, ['content-type' => 'application/json']);
+  }),
+  action('GET', '/users/:id', function ($args, $db) {
+    $user = loadUserById($db, $args['id']);
+    $html = phtml(__DIR__.'/views/user', ['user' => $user]);
+    return response($html);
+  }),
+  action('GET', '/about', page(__DIR__.'/views/about'))
+];
+
+# sample dependencies
+$config = require __DIR__.'/config.php';
+$settings = $config['settings'];
+$db = createDBConnection($settings['db']);
+
+# we need the method and requested path
+$verb = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['REQUEST_URI'];
+
+# serve app against verb + path, pass dependencies
+$responder = serve($routes, $verb, $path, $db, $config);
+
+# invoke responder to flush response
+$responder();
+
