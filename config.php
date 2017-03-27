2cc10f420c42231138bada229801f066767db80e
Mike Taylor <mike@whatsmycut.com>
2017-03-26 21:51:49 -0500

    localhost

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ return [
 */
         // DB settings
         'db' => [
-            'host' => "127.0.0.1",
+            'host' => "localhost",
             'user' => "dbo282171973",
             'pass' => "MySQL123!",
             'dbname' => "db282171973",
