0a833ffd03a57a3cf8f2b729b8745413a9b4eb13
Mike Taylor <mike@whatsmycut.com>
2017-04-06 22:43:53 -0500

    debug routes

--- index.php
+++ index.php
@@ -1,39 +1,30 @@
 <?php
 try {
-require 'dispatch.php';
-require 'wmcp_api.php';
+  require 'dispatch.php';
+  require 'wmcp_api.php';
 
-# load config
-$config = require __DIR__.'/config.php';
-$settings = $config['settings']['db'];
-$db = createDBConnection($settings);
-var_dump($db);
+  # load config
+  $config = require __DIR__.'/config.php';
+  $settings = $config['settings']['db'];
+  $db = createDBConnection($settings);
+  var_dump($db);
 
 
 # create a stack of actions
-$routes = [
-  action('GET', '/', function ($db, $config) {
+  route('GET', '/', function ($db, $config) {
     $list = "[{ 'status': '200', 'message': 'success'}]";
     $json = json_encode($list);
     return response($json, 200, ['content-type' => 'application/json']);
-  }),
-  action('GET', '/users/:id', function ($args, $db) {
+  });
+  route('GET', '/users/:id', function ($args, $db) {
     $user = loadUserById($db, $args['id']);
     $html = phtml(__DIR__.'/views/user', ['user' => $user]);
     return response($html);
-  }),
-  action('GET', '/aboutme', page(__DIR__.'/views/about'))
-];
+  });
+  route('GET', '/aboutme', page(__DIR__.'/views/about'));
 
-# we need the method and requested path
-$verb = $_SERVER['REQUEST_METHOD'];
-$path = $_SERVER['REQUEST_URI'];
+  dispatch($db, $config);
 
-# serve app against verb + path, pass dependencies
-$responder = serve($routes, $verb, $path, $db, $config);
-
-# invoke responder to flush response
-$responder();
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
