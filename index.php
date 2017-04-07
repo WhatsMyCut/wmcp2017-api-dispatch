7aaa9cfc55dbb00f10e9359a1463fe1d97112a40
Mike Taylor <mike@whatsmycut.com>
2017-04-06 22:45:31 -0500

    routes

--- index.php
+++ index.php
@@ -7,22 +7,21 @@ try {
   $config = require __DIR__.'/config.php';
   $settings = $config['settings']['db'];
   $db = createDBConnection($settings);
-  var_dump($db);
-
 
 # create a stack of actions
-  route('GET', '/', function ($db, $config) {
+  route('GET', '/home', function ($db, $config) {
     $list = "[{ 'status': '200', 'message': 'success'}]";
     $json = json_encode($list);
     return response($json, 200, ['content-type' => 'application/json']);
   });
+  /*
   route('GET', '/users/:id', function ($args, $db) {
     $user = loadUserById($db, $args['id']);
     $html = phtml(__DIR__.'/views/user', ['user' => $user]);
     return response($html);
   });
   route('GET', '/aboutme', page(__DIR__.'/views/about'));
-
+  */
   dispatch($db, $config);
 
 } catch (Exception $e) {
